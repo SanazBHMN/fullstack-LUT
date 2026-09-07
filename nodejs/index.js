29ec9b451cb32a53fc8bcd1acf82498735736803
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-09-07 12:21:08 +0300

    CommonJS modules

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const { generateRandomNumber } = require("./utils");
+const { celciusToFahrenheit } = require("./utils");
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`40 degrees celcius is ${celciusToFahrenheit(40)} fahrenheit`);
