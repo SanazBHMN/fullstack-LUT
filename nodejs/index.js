3b030b8cf4938df5e9e0d0763a3f5047ee26092c
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-09-07 12:28:16 +0300

    ES Modules

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,12 @@
-const { generateRandomNumber } = require("./utils");
-const { celciusToFahrenheit } = require("./utils");
+import getPosts, { getPostLength } from "./postController.js";
 
-console.log(`Random Number: ${generateRandomNumber()}`);
+console.log(getPosts());
 
-console.log(`40 degrees celcius is ${celciusToFahrenheit(40)} fahrenheit`);
+console.log(getPostLength());
+
+// const { generateRandomNumber } = require("./utils");
+// const { celciusToFahrenheit } = require("./utils");
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`40 degrees celcius is ${celciusToFahrenheit(40)} fahrenheit`);
