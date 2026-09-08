5b2e89d36a4ed037f933de5a03c36b87170f1a58
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-09-08 13:57:14 +0300

    Simple routing

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,13 +3,28 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  // res.setHeader("Content-Type", "text/html");
-  // res.statusCode = 404;
-
-  res.writeHead(500, {
-    "Content-Type": "application/json",
-  });
-  res.end(JSON.stringify({ message: "SERVER ERROR" }));
+  try {
+    // Check if GET request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, {
+          "Content-Type": "text/html",
+        });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About Page</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Page not found</h1>");
+      }
+    } else {
+      throw new Error("METHOD NOT ALLOWED");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("SERVER ERROR");
+  }
 });
 
 server.listen(PORT, () => {
