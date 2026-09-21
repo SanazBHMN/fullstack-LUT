16c3528648c5f5e116b8d5e3990cdf6eb06ea7cf
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-09-21 11:43:35 +0300

    Send html files as response

--- express/server.js
+++ express/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("HOME PAGE");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/about", (req, res) => {
-  res.send("ABOUT PAGE");
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
 app.listen(8000, () => {
