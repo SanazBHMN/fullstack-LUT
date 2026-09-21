f09f30132cc62f7a2690b5ce77537ed045a16a1a
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-09-21 14:06:44 +0300

    Environment variables

--- express/server.js
+++ express/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 
+const PORT = process.env.PORT || 8000;
+
 const app = express();
 
 // setup static folder
@@ -16,6 +18,6 @@ app.get("/api/posts", (req, res) => {
   res.json(posts);
 });
 
-app.listen(8000, () => {
-  console.log("THE SERVER IS RUNNING ON PORT 8000");
+app.listen(PORT, () => {
+  console.log(`THE SERVER IS RUNNING ON PORT ${PORT}`);
 });
