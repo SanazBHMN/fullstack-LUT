2652f9bb3b41500dcb75a739543518581a42f92c
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-09-21 17:24:11 +0300

    POST request

--- express/routes/posts.js
+++ express/routes/posts.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
   const limit = req.query.limit;
 
   if (!isNaN(limit) && limit > 0) {
-    return res.status(404).json(posts.slice(0, limit));
+    return res.status(401).json(posts.slice(0, limit));
   }
 
   res.status(200).json(posts);
@@ -31,6 +31,21 @@ router.get("/:id", (req, res) => {
   }
 
   res.status(200).json(post);
+});
+
+// Create a new post
+router.post("/", (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+  };
+
+  if (!newPost.title) {
+    return res.status(400).json({ message: `Please include a title` });
+  }
+
+  posts.push(newPost);
+  res.status(201).json(posts);
 });
 
 export default router;
