7fb008eec345ae2235f7e703aacc33d92f80fe8a
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-09-21 14:22:58 +0300

    Request params

--- express/server.js
+++ express/server.js
@@ -14,8 +14,16 @@ let posts = [
   { id: 3, title: "Post Three" },
 ];
 
+// Get all posts
 app.get("/api/posts", (req, res) => {
   res.json(posts);
+});
+
+// Get a single post
+app.get("/api/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+
+  res.json(posts.filter((post) => post.id === postId));
 });
 
 app.listen(PORT, () => {
