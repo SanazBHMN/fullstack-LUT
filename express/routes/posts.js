38ccf6b330233c217ca6b05cdb86b6b6a30d216c
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-09-23 11:48:13 +0300

    Middleware on route level

--- express/routes/posts.js
+++ express/routes/posts.js
@@ -8,8 +8,15 @@ let posts = [
   { id: 3, title: "Post Three" },
 ];
 
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`,
+  );
+  next();
+};
+
 // Get all posts
-router.get("/", (req, res) => {
+router.get("/", logger, (req, res) => {
   const limit = req.query.limit;
 
   if (!isNaN(limit) && limit > 0) {
